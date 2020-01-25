2e6134b506a7a1a424ef0bacd0d5751eaedcbb2f
Mehrad Rousta <mehrad77@gmail.com>
2020-01-25 15:33:50 +0100

    export section components

---
+++ src/sections/index.ts
@@ -0,0 +1,12 @@
+export { default as Attuned } from './Attuned';
+export { default as Background } from './Background';
+export { default as Cantrips } from './Cantrips';
+export { default as Character } from './Character';
+export { default as Equipment } from './Equipment';
+export { default as Feats } from './Feats';
+export { default as Gear } from './Gear';
+export { default as Money } from './Money';
+export { default as Skills } from './Skills';
+export { default as Spells } from './Spells';
+export { default as Vitals } from './Vitals';
+export { BardCF } from './classFeatures';
