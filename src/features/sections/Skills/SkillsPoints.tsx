11a71dba5f904c075c25ce97b8be99c629cb94c8
Mehrad Rousta <mehrad77@gmail.com>
2020-01-26 07:09:25 +0100

    add borders to SkillsPoints

--- src/features/sections/Skills/SkillsPoints.tsx
+++ src/features/sections/Skills/SkillsPoints.tsx
@@ -18,13 +18,18 @@ const SkillsPoints: FC<ISkillsPointsProps> = ({ abilities, skills, proficiencyBo
         const abilityScore = ability.baseNum + ability.addedNum;
         const abilityModifier = parseInt(String((abilityScore - 10) / 2));
 
+        let borderClass = 'border-gray-900';
         let skillModifier = abilityModifier;
         if (skill.proficiency === 'none') {
+          borderClass = 'border-none';
         } else if (skill.proficiency === 'half-proficient') {
+          borderClass = borderClass + ' border-dotted border-2';
           skillModifier = skillModifier + parseInt(String(proficiencyBonus / 2));
         } else if (skill.proficiency === 'proficient') {
+          borderClass = borderClass + ' border-solid border-2';
           skillModifier = skillModifier + proficiencyBonus;
         } else if (skill.proficiency === 'expert') {
+          borderClass = borderClass + ' border-solid border-4';
           skillModifier = skillModifier + proficiencyBonus * 2;
         }
         const sign =
@@ -32,11 +37,22 @@ const SkillsPoints: FC<ISkillsPointsProps> = ({ abilities, skills, proficiencyBo
 
         return (
           <div className="w-full sm:w-1/3 md:w-1/4">
-            <div className="flex justify-between items-baseline p-1 m-1 md:px-2 lg:px-4 bg-orange-600 text-gray-900 capitalize shadow-md">
-              <span className=" text-lg"> {skillName} </span>
-              <span className="text-2xl bg-orange-400 flex items-center justify-center rounded-full leading-none w-10 h-10 shadow">
-                {sign} {skillModifier}
-              </span>
+            <div
+              className="flex justify-between items-baseline p-1 m-1 md:px-2 lg:px-4
+            bg-orange-600 text-gray-900 capitalize shadow-md"
+            >
+              <div className="w-3/4 text-left">
+                <span> {skillName.replace(/([A-Z])/g, ' $1').trim()} </span>
+              </div>
+              <div className="w-1/4">
+                <span
+                  className={`text-2xl bg-orange-400 flex items-center
+                justify-center rounded-full leading-none w-10 h-10 shadow ${borderClass}`}
+                >
+                  {sign}
+                  {skillModifier}
+                </span>
+              </div>
             </div>
           </div>
         );
